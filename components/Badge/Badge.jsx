17659eb43d60f34b0ae8092153d329ef78b16284
Brock Penner <orbithammer@users.noreply.github.com>
2023-11-17 15:47:00 +0900

    Cleaning out repo

--- components/Badge/Badge.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function Badge({children, shape, color}) {
-    return (
-        <div className={`badge ${shape} badge-${color}`}>
-            {children}
-        </div>
-    )
-}
